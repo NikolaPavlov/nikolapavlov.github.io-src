36d2dbd9dbf416f07566106aac0f64228d652e50
NikolaPavlov <nikola.pavlov1955@gmail.com>
2016-07-12 12:39:20 +0300

    first commit source pelican blog

---
+++ content/first.md
@@ -0,0 +1,6 @@
+Title: first post
+Date: 2010-12-03 10:20
+Category: CTF
+
+
+this is the first post
---
+++ pelicanconf.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = 'me gogo'
+SITENAME = 'Test site'
+SITEURL = ''
+
+PATH = 'content'
+
+TIMEZONE = 'Europe/Paris'
+
+DEFAULT_LANG = 'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (('Pelican', '#'),
+         ('Jinja2', '#'),)
+
+# Social widget
+# SOCIAL = (('this is social link', '#'),
+#           ('Another social link', '#'),)
+
+DEFAULT_PAGINATION = 6
+
+# Uncomment following line if you want document-relative URLs when developing
+# RELATIVE_URLS = True
+
+# Theme settings
+THEME = 'pelican-blueidea'
+
+# Display pages list on the top menu
+DISPLAY_PAGES_ON_MENU=True
+
+# Display categories list on the top menu
+DISPLAY_CATEGORIES_ON_MENU=True
+
+# Display categories list as a submenu of the top menu
+DISPLAY_CATEGORIES_ON_SUBMENU=False
+
+# Display the category in the article's info
+DISPLAY_CATEGORIES_ON_POSTINFO=False
+
+# Display the author in the article's info
+DISPLAY_AUTHOR_ON_POSTINFO=False
+
+# Display the search form
+DISPLAY_SEARCH_FORM=True
+
+# Sort pages list by a given attribute
+# PAGES_SORT_ATTRIBUTE=Title
+
+# Display the "Fork me on Github" banner
+GITHUB_URL=None
+
+# Blogroll
+LINKS 
+
+# Social widget
+# SOCIAL
